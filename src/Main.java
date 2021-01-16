a1eded51696d4d4c941a8f2b49e62df06f4ba438
aymenkhs <aymenkhouas@gmail.com>
2021-01-16 13:25:45 +0100

    writing a pater to execute all what we need

--- src/Main.java
+++ src/Main.java
@@ -42,67 +42,56 @@ public class Main
 
 
     public static void main(String[] args) {
-        Scanner myScan = new Scanner(System.in);
-        System.out.println("Enter n (the number accepted must be in the form of: int*int^int)");
-        String unformated_n = myScan.nextLine();
-        System.out.println("your writed :" + unformated_n);
-
-        double n = format_n(unformated_n);
-        System.out.println("your number is :" + n);
-
-        long startTime = System.nanoTime();
-        double for_result = for_loop(n);
-        long endTime = System.nanoTime();
-        long duration = (endTime - startTime);
-        System.out.println("Le resultats obtenue pour la boucle for est : " + for_result + "\nLe temps necessaire est : " + duration);
-
-        startTime = System.nanoTime();
-        double while_result = while_loop(n);
-        endTime = System.nanoTime();
-        duration = (endTime - startTime);
-        System.out.println("Le resultats obtenue pour la boucle while est : " + while_result + "\nLe temps necessaire est : " + duration);
-
-        startTime = System.nanoTime();
-        double do_result = do_loop(n);
-        endTime = System.nanoTime();
-        duration = (endTime - startTime);
-        System.out.println("Le resultats obtenue pour la boucle do est : " + do_result + "\nLe temps necessaire est : " + duration);
 
 
-        try {
+        double multiplicateur = 1;
+        double integer = 10;
+        double puissance = 1;
+
+        while (puissance < 13){
+            double n = multiplicateur * (Math.pow(integer, puissance));
+            System.out.println("the actual number is : " + multiplicateur + "*" + integer + "^" + puissance +"\nor : " + n);
+
+            long startTime = System.nanoTime();
+            double for_result = for_loop(n);
+            long endTime = System.nanoTime();
+            long duration = (endTime - startTime);
+            System.out.println("Le resultats obtenue pour la boucle for est : " + for_result + "\nLe temps necessaire est : " + duration);
+
             startTime = System.nanoTime();
-            double rec_result = rec_sum(n);
+            double while_result = while_loop(n);
             endTime = System.nanoTime();
             duration = (endTime - startTime);
-            System.out.println("Le resultats obtenue pour la recursivité est : " + rec_result + "\nLe temps necessaire est : " + duration);
+            System.out.println("Le resultats obtenue pour la boucle while est : " + while_result + "\nLe temps necessaire est : " + duration);
+
+            startTime = System.nanoTime();
+            double do_result = do_loop(n);
+            endTime = System.nanoTime();
+            duration = (endTime - startTime);
+            System.out.println("Le resultats obtenue pour la boucle do est : " + do_result + "\nLe temps necessaire est : " + duration);
+
+
+            try {
+                startTime = System.nanoTime();
+                double rec_result = rec_sum(n);
+                endTime = System.nanoTime();
+                duration = (endTime - startTime);
+                System.out.println("Le resultats obtenue pour la recursivité est : " + rec_result + "\nLe temps necessaire est : " + duration);
+            }
+            catch(java.lang.StackOverflowError e) {
+                System.out.println("Le resultats n'a pas pu etre obtenue pour la recursivité a cause du depth recursion limite Le temps necessaire est indeterminer");
+            }
+
+
+            if (multiplicateur == 1){
+                multiplicateur = 2;
+            } else {
+                multiplicateur = 1;
+                puissance += 1;
+            }
+
+            System.out.println("\n\n\n");
         }
-        catch(java.lang.StackOverflowError e) {
-            System.out.println("Le resultats n'a pas pu etre obtenue pour la recursivité a cause du depth recursion limite Le temps necessaire est indeterminer");
-        }
-
-
-
     }
 
-    public static double format_n(String unformated_n){
-        double mult = 1, puiss = 1, integer;
-
-        String[] arrOfStr = unformated_n.split("\\*");
-        if (arrOfStr.length > 1){
-            mult = Double.parseDouble(arrOfStr[0]);
-            unformated_n = arrOfStr[1];
-        } else {
-            unformated_n = arrOfStr[0];
-        }
-
-        arrOfStr = unformated_n.split("\\^");
-        if (arrOfStr.length > 1){
-            integer = Double.parseDouble(arrOfStr[0]);
-            puiss = Double.parseDouble(arrOfStr[1]);
-        } else {
-            integer = Double.parseDouble(arrOfStr[0]);
-        }
-
-        return mult * (Math.pow(integer, puiss));
-    }
 }
